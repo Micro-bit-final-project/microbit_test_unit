0e8281445206d71a062514da03d5e872f2ae2a19
Giuseppe Barillari <joe2k01dev@gmail.com>
2020-12-13 15:38:42 +0000

    microbit_test_unit: Find the microbit's port 	* Using microbit_serial
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- microbit_test_unit.py
+++ microbit_test_unit.py
@@ -1,4 +1,11 @@
-import sys, pygame
+# Allow importing a file outside of python path
+import sys, os
+sys.path.append("../")
+# Import microbit serial handler
+from microbit_serial import microbit_serial as ubit
+
+import pygame, serial
+from time import sleep
 
 # Init pygame
 pygame.init()
@@ -7,6 +14,15 @@ pygame.init()
 screen = pygame.display.set_mode([800, 800])
 background = 30, 30, 30
 screen.fill(background)
+
+# Connect to the microbit
+port = ubit.connect()
+while type(port) is not serial.Serial:
+    print("Looking for a microbit")
+    port = ubit.connect()
+    sleep(1)
+
+print("Microbit Found")
 
 run = True
 while run:
